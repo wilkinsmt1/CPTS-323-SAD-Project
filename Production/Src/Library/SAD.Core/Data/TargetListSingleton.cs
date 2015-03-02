d959a0cf5d3a05ccbf5709854bcdbda261423824
Anna <annar.calvert@gmail.com>
2015-03-02 14:58:24 -0800

    added target list singleton, but not finished.

---
+++ Production/Src/Library/SAD.Core/Data/TargetListSingleton.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/*
+ * TargetListSingleton class:
+ * To get an instance of this class, use this:  TargetListSingleton targetListSingletonInstance = TargetListSingleton.GetInstance();  
+ * Singleton Info: p127 text; A singleton is used to allow only one instance of the class, and contain a global access point. 
+ * There should only be one list of targets; this will prevent multiple lists.
+ * This class may need modification; will be working on that. Let me know if there are any suggestions. -Anna
+ */
+
+namespace SAD.Core.Data
+{
+    public class TargetListSingleton
+    {
+        private static TargetListSingleton targetListSingletonInstance = null;
+
+        private TargetListSingleton(){}
+
+        public static TargetListSingleton GetInstance()
+        {
+            if (null == TargetListSingleton)   //error says is a type, but is used like a variable..
+            {
+                TargetListSingleton = new TargetListSingleton();
+            }
+            return TargetListSingleton;
+
+        //if TargetListSingleton ?? (TargetListSingleton = new TargetListSingleton());
+        //return TargetListSingleton;  //new instance assigned, and returned
+        }
+        public void WriteToConsole(string list)
+        {
+            Console.WriteLine(list);
+        } 
+      
+    };
+}
