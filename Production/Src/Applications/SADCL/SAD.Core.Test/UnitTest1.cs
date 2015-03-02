2540146c06cfc043948ce2d4301f44a00025dbe5
Anna <annar.calvert@gmail.com>
2015-03-02 09:04:36 -0800

    testing

---
+++ Production/Src/Applications/SADCL/SAD.Core.Test/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace SAD.Core.Test
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            var angler = new TargetPositionCalculator();
+
+            var angles = angler.Calculate(1, 2, 3);
+
+            Assert.IsTrue(angles.Phi - 56);
+            Assert.IsTrue(angles.Theta == 10);
+
+        }
+
+    }
+}
--- Production/Src/Applications/SADCL/SADCL/Program.cs
+++ Production/Src/Applications/SADCL/SADCL/Program.cs
@@ -4,13 +4,33 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace SADCL
+namespace SAD.Core.Test
 {
-    class Program
+    class UnitTest1
     {
-        static void Main(string[] args)
+        static void TestMethod2(string[] args)
         {
-            //change
+
+
+            double theta;
+            double phi;
+            MissileLauncher ml = new IMissileLauncher();
+            TargetPositioningAngles tpa = new TargetPositioningAngles(); //TargetPositioningAngles class, with spherical coordinates (rho, theta, phi)
+            Targets tar = new Targets();
+            MLFactory mlf = new MLFactory();
+            TargetPositioningAngles tp = new TargetPositioningAngles(); //TargetPositioning class, get theta, phi
+            UsbLibrary.Win32Usb usb = new UsbLibrary.Win32Usb();
+
+            theta = tp.CalculateRho(2.0, 3.0, 4.0);
+            phi = tp.CalculatePhi(2.0, 3.0, 4.0);
+            Console.WriteLine("Theta = " + theta);   //example: y=3; xadj=10
+            Console.WriteLine("Phi   = " + phi);
+            Console.Read();
+            //ML.MoveTo();
+            tar.internX(1);
+            tar.internY(1);
+            tar.internZ(1);
+            ml.Fire();//change
         }
     }
 }
