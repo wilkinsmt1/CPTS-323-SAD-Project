32703f3ea4920babc79afda62792d36a4e29e611
Michael Wilkins <michael.wilkins@email.wsu.edu>
2015-03-31 00:53:14 -0700

    Added data context and implemented the live video button

---
+++ Production/Src/Applications/SADGUI/MainViewModel.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Input;
+using System.Windows.Media.Imaging;
+using Emgu.CV;
+using Emgu.CV.Structure;
+using SADGUI.Annotations;
+
+namespace SADGUI
+{
+    class MainViewModel : ViewModelBase
+    {
+        private Capture m_camera;
+        private ICommand m_getImageCommand;
+        private BitmapImage m_source;
+        public MainViewModel()
+        {
+            m_camera = new Capture();
+            m_getImageCommand = new MyCommands(GetImage);
+            m_source = new BitmapImage();
+        }
+        private void GetImage()
+        {
+
+            Image<Bgr, byte> image = m_camera.QueryFrame(); //Take a frame with the webcam
+            Image windowsImage = image.ToBitmap(); //convert it to bitmap.
+            //windowsImage.Save("C:\\testfolder\\test.bmp", ImageFormat.Bmp);
+            //stream image to a memory stream and then save it
+            BitmapImage bImage = new BitmapImage();
+            bImage.BeginInit();
+            MemoryStream memStream = new MemoryStream();
+            windowsImage.Save(memStream, ImageFormat.Bmp);
+
+            //Set the memory stream back to the beginning
+            memStream.Seek(0, SeekOrigin.Begin);
+
+            //Set the stream source and assign it to pictureBox1's image source
+            bImage.StreamSource = memStream;
+            bImage.EndInit();
+            SourceImage = bImage;
+        }
+
+        public BitmapImage SourceImage
+        {
+            get { return m_source; }
+            set
+            {
+                m_source = value;
+                OnPropertyChanged();
+            }
+        }
+
+        public ICommand GetImageCommand
+        {
+            get { return m_getImageCommand; }
+            set
+            {
+                m_getImageCommand = value;
+                OnPropertyChanged();
+            }
+        }
+    }
+
+    public abstract class ViewModelBase : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        [NotifyPropertyChangedInvocator]
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChangedEventHandler handler = PropertyChanged;
+            if (handler != null)
+            {
+                handler(this, new PropertyChangedEventArgs(propertyName));
+            }
+        }
+    }
+}
---
+++ Production/Src/Applications/SADGUI/MyCommands.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Input;
+
+namespace SADGUI
+{
+    class MyCommands :ICommand
+    {
+        private Action m_action;
+
+        public MyCommands(Action action)
+        {
+            m_action = action;
+        }
+        public bool CanExecute(object parameter)
+        {
+            return true;
+        }
+
+        public void Execute(object parameter)
+        {
+            m_action();
+        }
+
+        public event EventHandler CanExecuteChanged;
+    }
+}
